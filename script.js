510c800e46f438853e0de9050eac0e9163dc46cd
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-03-23 11:01:15 +0100

    My JavaScript Task_1

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Benjamin Patrick Eyoh";
+console.log("My name is: " + name);
+let courses = ["HTML/CSS/JavaScript", " PHP", " C#", " Go"];
+console.log("My courses are: " + courses);
+
+const data = [];
+for (let i = 1; i <= 200; i++) {
+    if (i % 2 === 0) {
+        data.push(i);
+    }
+}
+console.log(data);
